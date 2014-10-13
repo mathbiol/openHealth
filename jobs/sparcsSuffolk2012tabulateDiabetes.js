09d5a395c5137a43b9e08b6de31a8dd0493ee481
Jonas Almeida <jalmeida@mathbiol.org>
2014-10-12 22:43:03 -0500

    Diabetes tabulation added

--- jobs/sparcsSuffolk2012tabulateDiabetes.js
+++ jobs/sparcsSuffolk2012tabulateDiabetes.js
@@ -1,7 +1,11 @@
 console.log("sparcsSuffolk2012.js loaded")
 
 sparcsSuffolk2012={};
-sparcsSuffolk2012.url="https://health.data.ny.gov/resource/u4ud-w55t.json?hospital_county=Suffolk&ccs_diagnosis_code=128";
+sparcsSuffolk2012.url=[
+	"https://health.data.ny.gov/resource/u4ud-w55t.json?hospital_county=Suffolk&ccs_diagnosis_code=186",
+	"https://health.data.ny.gov/resource/u4ud-w55t.json?hospital_county=Suffolk&ccs_diagnosis_code=050",
+	"https://health.data.ny.gov/resource/u4ud-w55t.json?hospital_county=Suffolk&ccs_diagnosis_code=049"
+	]
 sparcsSuffolk2012.loadData=function(fun){
 	if(!fun){fun=function(x){
 		sparcsSuffolk2012.docs=x;
@@ -9,7 +13,7 @@ sparcsSuffolk2012.loadData=function(fun){
 		sparcsSuffolk2012.attrs=Object.getOwnPropertyNames(sparcsSuffolk2012.tab)
 		console.log('.docs has '+sparcsSuffolk2012.docs.length+' entries')
 	}}
-	openHealth.sodaAll(sparcsSuffolk2012.url,false,fun)
+	openHealth.sodas(sparcsSuffolk2012.url,false,fun)
 
 	return this
 }
@@ -32,7 +36,7 @@ s.loadData(function(x){
 	})
 	s.medicaid.tab=openHealth.docs2tab(s.medicaid.docs)
 	console.log(s.medicaid.docs.length+" Medicaid entries found")
-	$("#openHealthJob").html('<p style="color:green">Ready to analyse the <b style="color:blue">'+s.docs.length+' Asthma</b> (ccs_diagnosis_code=128) entries in the public data <a href="https://health.data.ny.gov/Health/Hospital-Inpatient-Discharges-SPARCS-De-Identified/u4ud-w55t">"Hospital Inpatient Discharges (SPARCS De-Identified): 2012"</a> for Suffolk county, <b style="color:blue">'+s.medicaid.docs.length+'</b> of which from Medicaid patients</p>')
+	$("#openHealthJob").html('<p style="color:green">Ready to analyse the <b style="color:blue">'+s.docs.length+' Diabetes</b> (ccs_diagnosis_code=128) entries in the public data <a href="https://health.data.ny.gov/Health/Hospital-Inpatient-Discharges-SPARCS-De-Identified/u4ud-w55t">"Hospital Inpatient Discharges (SPARCS De-Identified): 2012"</a> for Suffolk county, <b style="color:blue">'+s.medicaid.docs.length+'</b> of which from Medicaid patients</p>')
 	$("#openHealthJob").append('<h4> Cross-tabulation counts for all entries and also for Medicat entries only: </h4>')
 	$("#openHealthJob").append('<p> Count <select id="parm1"></select> against <select id="parm2"></select><input id="tabulate" type="button" value="Tabulate"></p>')
 	s.Uparms = Object.getOwnPropertyNames(s.tab).sort();
@@ -40,24 +44,24 @@ s.loadData(function(x){
 		$('#parm1').append('<option value="'+u+'">'+u+'</option>')
 		$('#parm2').append('<option value="'+u+'">'+u+'</option>')
 	})
-	$("#openHealthJob").append('<hr><div id="allPatients"><h3>All Asthma patients</h3></div></hr><hr><div id="medicaidPatients"><h3>Medicaid Asthma patients</h3></div></hr>');
+	$("#openHealthJob").append('<hr><div id="allPatients"><h3>All Diabetes patients</h3></div></hr><hr><div id="medicaidPatients"><h3>Medicaid Diabetes patients</h3></div></hr>');
 	var bt = document.getElementById('tabulate')
 	document.getElementById('parm1').value='ccs_procedure_description'
 	document.getElementById('parm2').value='facility_name'
 	bt.onclick=function(){
-		$('#allPatients').html("<h3>All Asthma patients ("+s.docs.length+")</h3>")
-		$('#medicaidPatients').html("<h3>Medicaid Asthma patients ("+s.medicaid.docs.length+")</h3>")
+		$('#allPatients').html("<h3>All Diabetes patients ("+s.docs.length+")</h3>")
+		$('#medicaidPatients').html("<h3>Medicaid Diabetes patients ("+s.medicaid.docs.length+")</h3>")
 		var selParm1 = document.getElementById('parm1').value;
 		var selParm2 = document.getElementById('parm2').value;
 		var Up1 = openHealth.unique(s.tab[selParm1])
 		var Up2 = openHealth.unique(s.tab[selParm2])
 		$('#allPatients').append(openHealth.crossdoc2html(
 			openHealth.tabulateCount(s.tab,selParm1,selParm2,Up1,Up2),
-			'counts of '+selParm1+' vs '+selParm2+' for all Asthma patients.csv'
+			'counts of '+selParm1+' vs '+selParm2+' for all Diabetes patients.csv'
 		));
 		$('#medicaidPatients').append(openHealth.crossdoc2html(
 			openHealth.tabulateCount(s.medicaid.tab,selParm1,selParm2,Up1,Up2),
-			'counts of '+selParm1+' vs '+selParm2+' for Medicaid Asthma patients.csv'
+			'counts of '+selParm1+' vs '+selParm2+' for Medicaid Diabetes patients.csv'
 		));
 		
 		//console.log(Date(),this)
