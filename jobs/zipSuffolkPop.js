8f0355bd85f6856e115ef33e117c05b5476bdafa
Jonas Almeida <jalmeida@mathbiol.org>
2014-10-17 16:18:11 -0500

    population numbers

--- jobs/zipSuffolkPop.js
+++ jobs/zipSuffolkPop.js
@@ -1,114 +1,435 @@
 console.log("loaded Suffolk county population by zip using zipSuffolkPop.js")
-
+// http://api.census.gov/data/2010/sf1/variables.html
+//http://api.census.gov/data/2010/sf1?get=P0010001,P0030001,H00010001&for=zip+code+tabulation+area:*&in=state:36
 if(!openHealth.data.suffolkCounty){openHealth.data.suffolkCounty={}}
 
-openHealth.data.suffolkCounty.zipPop=
-{
- "11701": 27109,
- "11702": 14498,
- "11703": 16415,
- "11704": 40222,
- "11705": 8030,
- "11706": 62543,
- "11713": 10401,
- "11715": 4752,
- "11716": 10700,
- "11717": 60745,
- "11718": 3103,
- "11719": 3589,
- "11720": 29189,
- "11721": 6292,
- "11722": 35177,
- "11724": 3220,
- "11725": 29150,
- "11726": 19857,
- "11727": 29030,
- "11729": 28032,
- "11730": 14712,
- "11731": 30184,
- "11733": 18949,
- "11735": 32098,
- "11738": 17638,
- "11739": 1223,
- "11740": 9454,
- "11741": 27655,
- "11742": 13261,
- "11743": 42230,
- "11746": 67094,
- "11747": 19803,
- "11749": 3345,
- "11751": 14961,
- "11752": 9531,
- "11754": 18958,
- "11755": 12387,
- "11757": 45700,
- "11763": 28506,
- "11764": 13200,
- "11766": 12635,
- "11767": 14530,
- "11768": 21902,
- "11769": 9506,
- "11770": 137,
- "11772": 44837,
- "11776": 24235,
- "11777": 8968,
- "11778": 12709,
- "11779": 38421,
- "11780": 15524,
- "11782": 15814,
- "11784": 24655,
- "11786": 6344,
- "11787": 35772,
- "11788": 16821,
- "11789": 7895,
- "11790": 18511,
- "11792": 8523,
- "11794": 3165,
- "11795": 26030,
- "11796": 4056,
- "11798": 15347,
- "11901": 27172,
- "11930": 1343,
- "11931": 151,
- "11932": 1398,
- "11933": 6844,
- "11934": 7263,
- "11935": 3304,
- "11937": 15398,
- "11939": 926,
- "11940": 5210,
- "11941": 1986,
- "11942": 4603,
- "11944": 4299,
- "11946": 14068,
- "11947": 416,
- "11948": 1123,
- "11949": 14573,
- "11950": 16268,
- "11951": 13680,
- "11952": 4637,
- "11953": 13553,
- "11954": 3326,
- "11955": 3285,
- "11956": 349,
- "11957": 743,
- "11958": 635,
- "11959": 939,
- "11960": 1084,
- "11961": 12834,
- "11962": 494,
- "11963": 6622,
- "11964": 1858,
- "11965": 534,
- "11967": 26583,
- "11968": 11593,
- "11970": 522,
- "11971": 5818,
- "11972": 1321,
- "11973": 9,
- "11975": 408,
- "11976": 1565,
- "11977": 2467,
- "11978": 3255,
- "11980": 4990
+openHealth.data.suffolkCounty.zipPop={
+ "11701": {
+  "pop": 27109,
+  "name": "North Amityville"
+ },
+ "11702": {
+  "pop": 14498,
+  "name": "West Gilgo Beach"
+ },
+ "11703": {
+  "pop": 16415,
+  "name": "North Babylon"
+ },
+ "11704": {
+  "pop": 40222,
+  "name": "West Babylon"
+ },
+ "11705": {
+  "pop": 8030,
+  "name": "Bayport"
+ },
+ "11706": {
+  "pop": 62543,
+  "name": "West Bay Shore"
+ },
+ "11713": {
+  "pop": 10401,
+  "name": "North Bellport"
+ },
+ "11715": {
+  "pop": 4752,
+  "name": "Blue Point"
+ },
+ "11716": {
+  "pop": 10700,
+  "name": "Bohemia"
+ },
+ "11717": {
+  "pop": 60745,
+  "name": "West Brentwood"
+ },
+ "11718": {
+  "pop": 3103,
+  "name": "Brightwaters"
+ },
+ "11719": {
+  "pop": 3589,
+  "name": "South Haven"
+ },
+ "11720": {
+  "pop": 29189,
+  "name": "South Setauket"
+ },
+ "11721": {
+  "pop": 6292,
+  "name": "Centerport"
+ },
+ "11722": {
+  "pop": 35177,
+  "name": "South Hauppauge"
+ },
+ "11724": {
+  "pop": 3220,
+  "name": "Cold Spring Harbor"
+ },
+ "11725": {
+  "pop": 29150,
+  "name": "Commack"
+ },
+ "11726": {
+  "pop": 19857,
+  "name": "Marconiville"
+ },
+ "11727": {
+  "pop": 29030,
+  "name": "Coram"
+ },
+ "11729": {
+  "pop": 28032,
+  "name": "Deer Park"
+ },
+ "11730": {
+  "pop": 14712,
+  "name": "East Islip"
+ },
+ "11731": {
+  "pop": 30184,
+  "name": "Elwood"
+ },
+ "11733": {
+  "pop": 18949,
+  "name": "Strongs Neck"
+ },
+ "11735": {
+  "pop": 32098,
+  "name": "East Farmingdale"
+ },
+ "11738": {
+  "pop": 17638,
+  "name": "Farmingville"
+ },
+ "11739": {
+  "pop": 1223,
+  "name": "Great River"
+ },
+ "11740": {
+  "pop": 9454,
+  "name": "Greenlawn"
+ },
+ "11741": {
+  "pop": 27655,
+  "name": "Holbrook"
+ },
+ "11742": {
+  "pop": 13261,
+  "name": "Holtsville"
+ },
+ "11743": {
+  "pop": 42230,
+  "name": "Wincoma"
+ },
+ "11746": {
+  "pop": 67094,
+  "name": "South Huntington"
+ },
+ "11747": {
+  "pop": 19803,
+  "name": "Melville"
+ },
+ "11749": {
+  "pop": 3345,
+  "name": "Ronkonkoma"
+ },
+ "11751": {
+  "pop": 14961,
+  "name": "Islip Manor"
+ },
+ "11752": {
+  "pop": 9531,
+  "name": "Islip Terrace"
+ },
+ "11754": {
+  "pop": 18958,
+  "name": "San Remo"
+ },
+ "11755": {
+  "pop": 12387,
+  "name": "Lake Grove"
+ },
+ "11757": {
+  "pop": 45700,
+  "name": "North Lindenhurst"
+ },
+ "11763": {
+  "pop": 28506,
+  "name": "Medford"
+ },
+ "11764": {
+  "pop": 13200,
+  "name": "Miller Place"
+ },
+ "11766": {
+  "pop": 12635,
+  "name": "Mount Sinai"
+ },
+ "11767": {
+  "pop": 14530,
+  "name": "Nesconset"
+ },
+ "11768": {
+  "pop": 21902,
+  "name": "Sunken Meadow"
+ },
+ "11769": {
+  "pop": 9506,
+  "name": "Oakdale"
+ },
+ "11770": {
+  "pop": 137,
+  "name": "Seaview"
+ },
+ "11772": {
+  "pop": 44837,
+  "name": "Patchogue"
+ },
+ "11776": {
+  "pop": 24235,
+  "name": "Terryville"
+ },
+ "11777": {
+  "pop": 8968,
+  "name": "Port Jefferson Station"
+ },
+ "11778": {
+  "pop": 12709,
+  "name": "Rocky Point"
+ },
+ "11779": {
+  "pop": 38421,
+  "name": "Ronkonkoma"
+ },
+ "11780": {
+  "pop": 15524,
+  "name": "Saint James"
+ },
+ "11782": {
+  "pop": 15814,
+  "name": "Sayville"
+ },
+ "11784": {
+  "pop": 24655,
+  "name": "Selden"
+ },
+ "11786": {
+  "pop": 6344,
+  "name": "Shoreham"
+ },
+ "11787": {
+  "pop": 35772,
+  "name": "Smithtown"
+ },
+ "11788": {
+  "pop": 16821,
+  "name": "Smithtown"
+ },
+ "11789": {
+  "pop": 7895,
+  "name": "Sound Beach"
+ },
+ "11790": {
+  "pop": 18511,
+  "name": "Stony Brook"
+ },
+ "11792": {
+  "pop": 8523,
+  "name": "Wildwood"
+ },
+ "11794": {
+  "pop": 3165,
+  "name": "Stony Brook"
+ },
+ "11795": {
+  "pop": 26030,
+  "name": "West Islip"
+ },
+ "11796": {
+  "pop": 4056,
+  "name": "West Sayville"
+ },
+ "11798": {
+  "pop": 15347,
+  "name": "Wyandanch"
+ },
+ "11901": {
+  "pop": 27172,
+  "name": "Riverhead"
+ },
+ "11930": {
+  "pop": 1343,
+  "name": "Promised Land"
+ },
+ "11931": {
+  "pop": 151,
+  "name": "Aquebogue"
+ },
+ "11932": {
+  "pop": 1398,
+  "name": "Bridgehampton"
+ },
+ "11933": {
+  "pop": 6844,
+  "name": "Calverton"
+ },
+ "11934": {
+  "pop": 7263,
+  "name": "Center Moriches"
+ },
+ "11935": {
+  "pop": 3304,
+  "name": "Nassau Point"
+ },
+ "11937": {
+  "pop": 15398,
+  "name": "East Hampton"
+ },
+ "11939": {
+  "pop": 926,
+  "name": "East Marion"
+ },
+ "11940": {
+  "pop": 5210,
+  "name": "East Moriches"
+ },
+ "11941": {
+  "pop": 1986,
+  "name": "Eastport"
+ },
+ "11942": {
+  "pop": 4603,
+  "name": "East Quogue"
+ },
+ "11944": {
+  "pop": 4299,
+  "name": "Greenport"
+ },
+ "11946": {
+  "pop": 14068,
+  "name": "Hampton Bays"
+ },
+ "11947": {
+  "pop": 416,
+  "name": "Jamesport"
+ },
+ "11948": {
+  "pop": 1123,
+  "name": "Laurel"
+ },
+ "11949": {
+  "pop": 14573,
+  "name": "Manorville"
+ },
+ "11950": {
+  "pop": 16268,
+  "name": "Rivers Edge"
+ },
+ "11951": {
+  "pop": 13680,
+  "name": "Old Mastic"
+ },
+ "11952": {
+  "pop": 4637,
+  "name": "Mattituck"
+ },
+ "11953": {
+  "pop": 13553,
+  "name": "Middle Island"
+ },
+ "11954": {
+  "pop": 3326,
+  "name": "Montauk"
+ },
+ "11955": {
+  "pop": 3285,
+  "name": "Moriches"
+ },
+ "11956": {
+  "pop": 349,
+  "name": "New Suffolk"
+ },
+ "11957": {
+  "pop": 743,
+  "name": "Orient Point"
+ },
+ "11958": {
+  "pop": 635,
+  "name": "Peconic"
+ },
+ "11959": {
+  "pop": 939,
+  "name": "Quogue"
+ },
+ "11960": {
+  "pop": 1084,
+  "name": "Remsenburg"
+ },
+ "11961": {
+  "pop": 12834,
+  "name": "Ridge"
+ },
+ "11962": {
+  "pop": 494,
+  "name": "Sagaponack"
+ },
+ "11963": {
+  "pop": 6622,
+  "name": "Sag Harbor"
+ },
+ "11964": {
+  "pop": 1858,
+  "name": "Shelter Island"
+ },
+ "11965": {
+  "pop": 534,
+  "name": "Shelter Island Heights"
+ },
+ "11967": {
+  "pop": 26583,
+  "name": "Shirley"
+ },
+ "11968": {
+  "pop": 11593,
+  "name": "Southampton"
+ },
+ "11970": {
+  "pop": 522,
+  "name": "South Jamesport"
+ },
+ "11971": {
+  "pop": 5818,
+  "name": "Southold"
+ },
+ "11972": {
+  "pop": 1321,
+  "name": "Speonk"
+ },
+ "11973": {
+  "pop": 9,
+  "name": "Upton"
+ },
+ "11975": {
+  "pop": 408,
+  "name": "Wainscott"
+ },
+ "11976": {
+  "pop": 1565,
+  "name": "Water Mill"
+ },
+ "11977": {
+  "pop": 2467,
+  "name": "Westhampton"
+ },
+ "11978": {
+  "pop": 3255,
+  "name": "Westhampton Dunes"
+ },
+ "11980": {
+  "pop": 4990,
+  "name": "Yaphank"
+ }
 }
